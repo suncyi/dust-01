a415aed834cf3833a66d4a89f4fbc7bdc2f861ca
sunchangyu <sunchangyu@liepin.com>
2020-08-15 14:45:39 +0800

    Initial commit

---
+++ dust-01/src/main/java/net/suncy/dust01/Dust01Application.java
@@ -0,0 +1,13 @@
+package net.suncy.dust01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dust01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dust01Application.class, args);
+	}
+
+}
---
+++ dust-01/src/main/java/net/suncy/dust01/recursion/Rec01.java
@@ -0,0 +1,8 @@
+package net.suncy.dust01.recursion;
+
+/**
+ * @auther suncy
+ * @Date 2020/8/15  14:07
+ */
+public class Rec01 {
+}
